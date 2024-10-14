4f4e8be1bf0bf633fef578186a85738b298d9610
Morkovka21Vek <142694961+Morkovka21Vek@users.noreply.github.com>
2024-10-14 21:50:31 +0300

    add fast_start_code.cpp

---
+++ fast_start_code.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int args_count, char *args[]) {
+
+	//std::cout << args[1] << std::endl;
+	//std::cout << args[2] << std::endl;
+
+	system(args[1]);
+	system(args[2]);
+
+	return 0;
+}

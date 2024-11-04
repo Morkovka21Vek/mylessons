02437b97c2fdf00c638bd63991ee3822731b5353
Morkovka21Vek <142694961+Morkovka21Vek@users.noreply.github.com>
2024-11-04 23:04:56 +0300

    Add scripts and fixes

---
+++ getmonth.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+
+int printMonths(int month_num){
+	int result = 0;
+  const char * monts[] = {
+	 "Январь",	
+	 "Февраль",	
+	 "Март",	
+	 "Апрель",	
+	 "Май",	
+	 "Июнь",	
+	 "Июль",	
+	 "Август",	
+	 "Сентябрь",	
+	 "Октябрь",	
+	 "Ноябрь",	
+	 "Декарь"	
+	};
+  const int monts_len = sizeof(monts) / sizeof(char*);
+	
+	if (!((month_num - 1) >= 0 && (month_num < monts_len))){
+		result = -1;
+	}
+	else {
+    std::cout << monts[month_num - 1] << std::endl;
+	}
+  return (result);
+}
+
+int main() {
+	int number;
+
+	std::cout << "Введите пожалуйста номер месяца от 1 до 12:\n>>>";
+	std::cin >> number;
+
+	std::cout << "Это месяц: ";
+  printMonths(number);
+	return 0;
+	switch (number)
+	{
+		case 1:
+			std::cout << "Январь";
+			break;
+		case 2:
+			std::cout << "Февраль" ;
+			break;
+		case 3:
+			std::cout << "Март";
+			break;
+		case 4:
+			std::cout << "Апрель";
+			break;
+		case 5:
+			std::cout << "Май";
+			break;
+		case 6:
+			std::cout << "Июнь";
+			break;
+		case 7:
+			std::cout << "Июль";
+			break;
+		case 8:
+			std::cout << "Август";
+			break;
+		case 9:
+			std::cout << "Сентябрь";
+			break;
+		case 10:
+			std::cout << "Октябрь";
+			break;
+		case 11:
+			std::cout << "Ноябрь";
+			break;
+		case 12:
+			std::cout << "Декабрь";
+			break;
+		default:
+			std::cout << "Неверный номер месяца!";
+	}
+	std::cout << std::endl;
+
+}

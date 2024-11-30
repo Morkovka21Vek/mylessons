472b3d35d475e72ab57319e34a0f2926e1a2c8e3
Morkovka21Vek <142694961+Morkovka21Vek@users.noreply.github.com>
2024-11-30 12:39:10 +0300

    Update ping_pong (one file to all versions)

---
+++ ping_pong/assets.h
@@ -0,0 +1,23 @@
+enum charsEnum {
+  Char_ball,
+  Char_0,
+  Char_1,
+  Char_2,
+  Char_3,
+  Char_colon, //двоеточие
+  Char_P,
+  Char_L,
+  Char_A,
+  Char_Y,
+  Char_E,
+  Char_R,
+  Char_O,
+  Char_I,
+  Char_N,
+  Char_T,
+  Char_W,
+  Char_plus
+};
+char getSymbolCh (enum charsEnum ch, int, int);
+void getSizeCh (enum charsEnum ch, int& xSize, int& ySize);
+

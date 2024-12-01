27e35b152ce63e3708012d78db107c9dab8660bb
Morkovka21Vek <142694961+Morkovka21Vek@users.noreply.github.com>
2024-12-01 13:55:35 +0300

    Update ping_pong in class

--- ping_pong/ping_buttons/main/ping_buttons.c
+++ ping_pong/ping_buttons/main/ping_buttons.c
@@ -8,10 +8,10 @@
 
 #define but_right_down_pin 2
 #define but_right_up_pin 3
+#define print_symbol_by_pin(pin,symbol) {if(!gpio_get_level(pin)) printf(symbol);}
 
 void app_main(void)
 {
-  //gpio_pad_select_gpio(but_left_pin);
   gpio_set_direction(but_left_down_pin, GPIO_MODE_INPUT);
   gpio_set_pull_mode(but_left_down_pin, GPIO_PULLUP_ONLY);
 
@@ -28,22 +28,20 @@ void app_main(void)
   fflush(stdout);
 
   while(1) {
-    //int level = gpio_get_level(but_left_down_pin);
-    //printf("%d ", level);
-    if(!gpio_get_level(but_left_down_pin))
-      printf("z");
-    if(!gpio_get_level(but_left_up_pin))
-      printf("a");
 
-    if(!gpio_get_level(but_right_down_pin))
-      printf("/");
-    if(!gpio_get_level(but_right_up_pin))
-      printf("\'");
+    print_symbol_by_pin(but_left_down_pin,  "z");
+    print_symbol_by_pin(but_left_up_pin,    "a");
+    print_symbol_by_pin(but_right_down_pin, "/");
+    print_symbol_by_pin(but_right_up_pin,   "\'");
 
     fflush(stdout);
 
-    if(!gpio_get_level(but_left_down_pin) || !gpio_get_level(but_left_up_pin)
-        || !gpio_get_level(but_right_down_pin) || !gpio_get_level(but_right_up_pin))
+    if(1 || !gpio_get_level(but_left_down_pin) 
+         || !gpio_get_level(but_left_up_pin)
+         || !gpio_get_level(but_right_down_pin) 
+         || !gpio_get_level(but_right_up_pin)){
+
       vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
   }
 }

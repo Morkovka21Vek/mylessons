e8889451b67dcdbbb3d657dad0aae535afcc376f
Morkovka21Vek <142694961+Morkovka21Vek@users.noreply.github.com>
2024-12-11 19:43:08 +0300

    update ping_pong

--- ping_pong/http.h
+++
@@ -1,2 +0,0 @@
-void getHttpBtnCout(player& pl1, player& pl2, int windowWidth, int windowHeight);
-
---
+++ ping_pong/inputs.h
@@ -0,0 +1,20 @@
+#include <vector>
+
+struct prediction {
+  float pred;
+  int predTime;
+  std::vector<int> pathX;
+  std::vector<int> pathY;
+};
+
+prediction calcPred(struct square sqrPred, int leftMargin, int rightMargin, int width, int height);
+void botTick(struct player& pl, square, int windowWidth, int windowHeight);
+
+
+void keyInpTick (player& pl1, char up_key1, char down_key1, player& pl2, char up_key2, char down_key2, int windowWidth, int windowHeight);
+
+
+void serial_stdTick (player& pl1, char up_key1, char down_key1, player& pl2, char up_key2, char down_key2, int windowWidth, int windowHeight);
+
+
+void getHttpBtnCout(player& pl1, player& pl2, int windowWidth, int windowHeight);

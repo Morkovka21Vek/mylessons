ec7ff020e24450dc8a1e548e9ea78bafea1e3a84
Morkovka21Vek <142694961+Morkovka21Vek@users.noreply.github.com>
2024-12-11 19:13:03 +0300

    add socket connection

--- ping_pong/http.cpp
+++ ping_pong/http.cpp
@@ -1,26 +1,44 @@
 #include "assets.h"
-#include <cpr/cpr.h>
+//#include <cpr/cpr.h>
 #include <string>
 #include <iostream>
 #include <sstream>
+#include "Socket.h"
 
 
 void getHttpBtnCout(player& pl1, player& pl2, int windowWidth, int windowHeight) {
-  cpr::Response r = cpr::Get(cpr::Url{"http://192.168.4.1:80/api/get_buttons_count"});
-  //cpr::Response r = cpr::Get(cpr::Url{"http://www.httpbin.org/get"});
+  //cpr::Response r = cpr::Get(cpr::Url{"http://192.168.4.1:80/api/get_buttons_count"});
+  Connection conn("192.168.4.1", 80);
 
   int val1 = 0, val2 = 0;
 
+  conn.tx("GET /api/get_buttons_count HTTP/1.1\r\nHost: 192.168.4.1\r\n\r\n");
+  std::string s = conn.rx();
+
+  conn.tx("GET /api/get_buttons_count HTTP/1.1\r\nHost: 192.168.4.1\r\n\r\n");
+  s = conn.rx();
+  std::cerr << s << std::endl;
+  
+  std::string body = s;
+
   std::string parsed;
-  std::stringstream input_stringstream(static_cast<std::string>(r.text));
+  std::stringstream input_stringstream(static_cast<std::string>(body));
 
   if (getline(input_stringstream,parsed,';')) {
-    //std::cerr << parsed << std::endl;
-    val1 = std::stoi(parsed);
+    std::cerr << parsed << std::endl;
+    try { 
+      val1 = std::stoi(parsed);
+    } catch(...) {
+      std::cerr << "ERROR" << std::endl;
+    }
   }
   if (getline(input_stringstream,parsed,';')) {
-    //std::cerr << parsed << std::endl;
-    val2 = std::stoi(parsed);
+    std::cerr << parsed << std::endl;
+    try { 
+      val2 = std::stoi(parsed);
+    } catch(...) {
+      std::cerr << "ERROR" << std::endl;
+    }
   }
 
   pl1.pos += val1;

44e2a9aec8a24ca60f5b00e238c54f13f3505179
Morkovka21Vek <142694961+Morkovka21Vek@users.noreply.github.com>
2024-12-17 20:21:36 +0300

    Update simple_http_server

--- ping_pong/esp/simple_http_server/main/main.c
+++ ping_pong/esp/simple_http_server/main/main.c
@@ -20,7 +20,6 @@
 #include "esp_eth.h"
 #endif  // !CONFIG_IDF_TARGET_LINUX
 
-//My
 
 #include "htmls.h"
 
@@ -69,7 +68,7 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base,
 void wifi_init_softap(void)
 {
     ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    //ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_create_default_wifi_ap();
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
@@ -88,12 +87,7 @@ void wifi_init_softap(void)
             .channel = EXAMPLE_ESP_WIFI_CHANNEL,
             .password = EXAMPLE_ESP_WIFI_PASS,
             .max_connection = EXAMPLE_MAX_STA_CONN,
-//#ifdef CONFIG_ESP_WIFI_SOFTAP_SAE_SUPPORT
-//            .authmode = WIFI_AUTH_WPA3_PSK,
-//            .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
-//#else /* CONFIG_ESP_WIFI_SOFTAP_SAE_SUPPORT */
             .authmode = WIFI_AUTH_OPEN,
-//#endif
             .pmf_cfg = {
                     .required = true,
             },
@@ -111,235 +105,7 @@ void wifi_init_softap(void)
              EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS, EXAMPLE_ESP_WIFI_CHANNEL);
 }
 
-static esp_err_t hello_get_handler(httpd_req_t *req)
-{
-    char*  buf;
-    size_t buf_len;
-
-    /* Get header value string length and allocate memory for length + 1,
-     * extra byte for null termination */
-    buf_len = httpd_req_get_hdr_value_len(req, "Host") + 1;
-    if (buf_len > 1) {
-        buf = malloc(buf_len);
-        /* Copy null terminated value string into buffer */
-        if (httpd_req_get_hdr_value_str(req, "Host", buf, buf_len) == ESP_OK) {
-            ESP_LOGI(TAG, "Found header => Host: %s", buf);
-        }
-        free(buf);
-    }
-
-    buf_len = httpd_req_get_hdr_value_len(req, "Test-Header-2") + 1;
-    if (buf_len > 1) {
-        buf = malloc(buf_len);
-        if (httpd_req_get_hdr_value_str(req, "Test-Header-2", buf, buf_len) == ESP_OK) {
-            ESP_LOGI(TAG, "Found header => Test-Header-2: %s", buf);
-        }
-        free(buf);
-    }
-
-    buf_len = httpd_req_get_hdr_value_len(req, "Test-Header-1") + 1;
-    if (buf_len > 1) {
-        buf = malloc(buf_len);
-        if (httpd_req_get_hdr_value_str(req, "Test-Header-1", buf, buf_len) == ESP_OK) {
-            ESP_LOGI(TAG, "Found header => Test-Header-1: %s", buf);
-        }
-        free(buf);
-    }
-
-    /* Read URL query string length and allocate memory for length + 1,
-     * extra byte for null termination */
-    buf_len = httpd_req_get_url_query_len(req) + 1;
-    if (buf_len > 1) {
-        buf = malloc(buf_len);
-        if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK) {
-            ESP_LOGI(TAG, "Found URL query => %s", buf);
-            char param[EXAMPLE_HTTP_QUERY_KEY_MAX_LEN], dec_param[EXAMPLE_HTTP_QUERY_KEY_MAX_LEN] = {0};
-            /* Get value of expected key from query string */
-            if (httpd_query_key_value(buf, "query1", param, sizeof(param)) == ESP_OK) {
-                ESP_LOGI(TAG, "Found URL query parameter => query1=%s", param);
-                example_uri_decode(dec_param, param, strnlen(param, EXAMPLE_HTTP_QUERY_KEY_MAX_LEN));
-                ESP_LOGI(TAG, "Decoded query parameter => %s", dec_param);
-            }
-            if (httpd_query_key_value(buf, "query3", param, sizeof(param)) == ESP_OK) {
-                ESP_LOGI(TAG, "Found URL query parameter => query3=%s", param);
-                example_uri_decode(dec_param, param, strnlen(param, EXAMPLE_HTTP_QUERY_KEY_MAX_LEN));
-                ESP_LOGI(TAG, "Decoded query parameter => %s", dec_param);
-            }
-            if (httpd_query_key_value(buf, "query2", param, sizeof(param)) == ESP_OK) {
-                ESP_LOGI(TAG, "Found URL query parameter => query2=%s", param);
-                example_uri_decode(dec_param, param, strnlen(param, EXAMPLE_HTTP_QUERY_KEY_MAX_LEN));
-                ESP_LOGI(TAG, "Decoded query parameter => %s", dec_param);
-            }
-        }
-        free(buf);
-    }
-
-    /* Set some custom headers */
-    httpd_resp_set_hdr(req, "Custom-Header-1", "Custom-Value-1");
-    httpd_resp_set_hdr(req, "Custom-Header-2", "Custom-Value-2");
-
-    /* Send response with custom headers and body set as the
-     * string passed in user context*/
-    const char* resp_str = (const char*) req->user_ctx;
-    httpd_resp_send(req, resp_str, HTTPD_RESP_USE_STRLEN);
-
-    /* After sending the HTTP response the old HTTP request
-     * headers are lost. Check if HTTP request headers can be read now. */
-    if (httpd_req_get_hdr_value_len(req, "Host") == 0) {
-        ESP_LOGI(TAG, "Request headers lost");
-    }
-    return ESP_OK;
-}
-
-static const httpd_uri_t hello = {
-    .uri       = "/hello",
-    .method    = HTTP_GET,
-    .handler   = hello_get_handler,
-    /* Let's pass response string in user
-     * context to demonstrate it's usage */
-    .user_ctx  = "Hello World!"
-};
-
-/* An HTTP POST handler */
-
-static esp_err_t html_get_handler(httpd_req_t *req)
-{
-    /*const char* resp_str = (const char*) req->user_ctx;*/
-
-    httpd_resp_send(req, fontPage, HTTPD_RESP_USE_STRLEN);
-
-    if (httpd_req_get_hdr_value_len(req, "Host") == 0) {
-        ESP_LOGI(TAG, "Request headers lost");
-    }
-    return ESP_OK;
-}
-
-static const httpd_uri_t html_struct = {
-    .uri       = "/html",
-    .method    = HTTP_GET,
-    .handler   = html_get_handler,
-};
-
-static esp_err_t api_get_status_buttons_get_handler(httpd_req_t *req)
-{
-    char resp_json[100];
-
-    snprintf(resp_json, sizeof(resp_json), "{\n  \"btn1\": %d,\n  \"btn2\": %d\n}", 
-        !gpio_get_level(but_left_down_pin), 
-        !gpio_get_level(but_left_up_pin));
-
-    httpd_resp_set_type(req, HTTPD_TYPE_JSON);
-    httpd_resp_send(req, resp_json, HTTPD_RESP_USE_STRLEN);
-
-    if (httpd_req_get_hdr_value_len(req, "Host") == 0) {
-        ESP_LOGI(TAG, "Request headers lost");
-    }
-    return ESP_OK;
-}
-
-static const httpd_uri_t api_get_status_buttons_struct = {
-    .uri       = "/api/get_buttons_status",
-    .method    = HTTP_GET,
-    .handler   = api_get_status_buttons_get_handler,
-};
-
-static esp_err_t api_get_count_buttons_get_handler(httpd_req_t *req)
-{
-    char resp_json[100];
-
-    snprintf(resp_json, sizeof(resp_json), "%d;%d", 
-        button_count1, 
-        button_count2);
-    
-    button_count1 = button_count2 = 0;
-
-    httpd_resp_set_type(req, HTTPD_TYPE_JSON);
-    httpd_resp_send(req, resp_json, HTTPD_RESP_USE_STRLEN);
-
-    if (httpd_req_get_hdr_value_len(req, "Host") == 0) {
-        ESP_LOGI(TAG, "Request headers lost");
-    }
-    return ESP_OK;
-}
-
-static const httpd_uri_t api_get_count_buttons_struct = {
-    .uri       = "/api/get_buttons_count",
-    .method    = HTTP_GET,
-    .handler   = api_get_count_buttons_get_handler,
-};
-
-static esp_err_t home_get_handler(httpd_req_t *req)
-{
-    /* const char* resp_str = (const char*) req->user_ctx; */
-    httpd_resp_send(req, mainPage, HTTPD_RESP_USE_STRLEN);
-
-    if (httpd_req_get_hdr_value_len(req, "Host") == 0) {
-        ESP_LOGI(TAG, "Request headers lost");
-    }
-    return ESP_OK;
-}
-
-static const httpd_uri_t home_struct = {
-    .uri       = "/",
-    .method    = HTTP_GET,
-    .handler   = home_get_handler,
-    .user_ctx  = "Ok!"
-};
-
-/* An HTTP POST handler */
-static esp_err_t echo_post_handler(httpd_req_t *req)
-{
-    char buf[100];
-    int ret, remaining = req->content_len;
-
-    while (remaining > 0) {
-        /* Read the data for the request */
-        if ((ret = httpd_req_recv(req, buf,
-                        MIN(remaining, sizeof(buf)))) <= 0) {
-            if (ret == HTTPD_SOCK_ERR_TIMEOUT) {
-                /* Retry receiving if timeout occurred */
-                continue;
-            }
-            return ESP_FAIL;
-        }
-
-        /* Send back the same data */
-        httpd_resp_send_chunk(req, buf, ret);
-        remaining -= ret;
-
-        /* Log data received */
-        ESP_LOGI(TAG, "=========== RECEIVED DATA ==========");
-        ESP_LOGI(TAG, "%.*s", ret, buf);
-        ESP_LOGI(TAG, "====================================");
-    }
-
-    // End response
-    httpd_resp_send_chunk(req, NULL, 0);
-    return ESP_OK;
-}
-
-static const httpd_uri_t echo = {
-    .uri       = "/echo",
-    .method    = HTTP_POST,
-    .handler   = echo_post_handler,
-    .user_ctx  = NULL
-};
-
-esp_err_t http_404_error_handler(httpd_req_t *req, httpd_err_code_t err)
-{
-    if (strcmp("/hello", req->uri) == 0) {
-        httpd_resp_send_err(req, HTTPD_404_NOT_FOUND, "/hello URI is not available");
-        /* Return ESP_OK to keep underlying socket open */
-        return ESP_OK;
-    } else if (strcmp("/echo", req->uri) == 0) {
-        httpd_resp_send_err(req, HTTPD_404_NOT_FOUND, "/echo URI is not available");
-        /* Return ESP_FAIL to close underlying socket */
-        return ESP_FAIL;
-    }
-    /* For any other URI send 404 and close socket */
-    httpd_resp_send_err(req, HTTPD_404_NOT_FOUND, "Some 404 error message");
-    return ESP_FAIL;
-}
+#include "pages.c"
 
 /* An HTTP PUT handler. This demonstrates realtime
  * registration and deregistration of URI handlers
@@ -358,10 +124,6 @@ static esp_err_t ctrl_put_handler(httpd_req_t *req)
 
     if (buf == '0') {
         /* URI handlers can be unregistered using the uri string */
-        ESP_LOGI(TAG, "Unregistering /hello and /echo URIs");
-        httpd_unregister_uri(req->handle, "/hello");
-        httpd_unregister_uri(req->handle, "/echo");
-        httpd_unregister_uri(req->handle, "/html");
         httpd_unregister_uri(req->handle, "/");
         httpd_unregister_uri(req->handle, "/api/get_buttons_status");
         httpd_unregister_uri(req->handle, "/api/get_buttons_count");
@@ -369,10 +131,6 @@ static esp_err_t ctrl_put_handler(httpd_req_t *req)
         httpd_register_err_handler(req->handle, HTTPD_404_NOT_FOUND, http_404_error_handler);
     }
     else {
-        ESP_LOGI(TAG, "Registering /hello and /echo URIs");
-        httpd_register_uri_handler(req->handle, &hello);
-        httpd_register_uri_handler(req->handle, &echo);
-        httpd_register_uri_handler(req->handle, &html_struct);
         httpd_register_uri_handler(req->handle, &home_struct);
         httpd_register_uri_handler(req->handle, &api_get_status_buttons_struct);
         httpd_register_uri_handler(req->handle, &api_get_count_buttons_struct);
@@ -406,14 +164,11 @@ static httpd_handle_t start_webserver(void)
     config.lru_purge_enable = true;
 
     // Start the httpd server
-    ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
+    ESP_LOGD(TAG, "Starting server on port: '%d'", config.server_port);
     if (httpd_start(&server, &config) == ESP_OK) {
         // Set URI handlers
-        ESP_LOGI(TAG, "Registering URI handlers");
-        httpd_register_uri_handler(server, &hello);
-        httpd_register_uri_handler(server, &echo);
+        ESP_LOGD(TAG, "Registering URI handlers");
         httpd_register_uri_handler(server, &ctrl);
-        httpd_register_uri_handler(server, &html_struct);
         httpd_register_uri_handler(server, &home_struct);
         httpd_register_uri_handler(server, &api_get_status_buttons_struct);
         httpd_register_uri_handler(server, &api_get_count_buttons_struct);
@@ -423,7 +178,7 @@ static httpd_handle_t start_webserver(void)
         return server;
     }
 
-    ESP_LOGI(TAG, "Error starting server!");
+    ESP_LOGD(TAG, "Error starting server!");
     return NULL;
 }
 
@@ -439,7 +194,7 @@ static void disconnect_handler(void* arg, esp_event_base_t event_base,
 {
     httpd_handle_t* server = (httpd_handle_t*) arg;
     if (*server) {
-        ESP_LOGI(TAG, "Stopping webserver");
+        ESP_LOGD(TAG, "Stopping webserver");
         if (stop_webserver(*server) == ESP_OK) {
             *server = NULL;
         } else {
@@ -453,18 +208,11 @@ static void connect_handler(void* arg, esp_event_base_t event_base,
 {
     httpd_handle_t* server = (httpd_handle_t*) arg;
     if (*server == NULL) {
-        ESP_LOGI(TAG, "Starting webserver");
+        ESP_LOGD(TAG, "Starting webserver");
         *server = start_webserver();
     }
 }
 #endif // !CONFIG_IDF_TARGET_LINUX
-
-//void button_tick() {
-//  if (!gpio_get_level(but_left_down_pin) ||
-//      !gpio_get_level(but_left_up_pin)) gpio_set_level(3, 1);
-//  
-//  else gpio_set_level(3, 0);
-//}
 
 void IRAM_ATTR button_tick1(void* arg) {
   uint32_t current_time = esp_timer_get_time() / 1000;
@@ -472,8 +220,6 @@ void IRAM_ATTR button_tick1(void* arg) {
     button_count1++;
     last_interrupt_time1 = current_time;
   }
-  //printf("btn1");
-  //fflush(stdout);
 }
 void IRAM_ATTR button_tick2(void* arg) {
   uint32_t current_time = esp_timer_get_time() / 1000;
@@ -485,7 +231,7 @@ void IRAM_ATTR button_tick2(void* arg) {
 
 void app_main(void)
 {
-    
+    // Pin setup 
     gpio_set_direction(but_left_down_pin, GPIO_MODE_INPUT);
     gpio_set_pull_mode(but_left_down_pin, GPIO_PULLUP_ONLY);
     gpio_set_intr_type(but_left_down_pin, GPIO_INTR_NEGEDGE);
@@ -493,8 +239,6 @@ void app_main(void)
     gpio_set_direction(but_left_up_pin, GPIO_MODE_INPUT);
     gpio_set_pull_mode(but_left_up_pin, GPIO_PULLUP_ONLY);
     gpio_set_intr_type(but_left_up_pin, GPIO_INTR_NEGEDGE);
-
-    gpio_set_direction(3, GPIO_MODE_OUTPUT);
 
     gpio_install_isr_service(0);
     gpio_isr_handler_add(but_left_down_pin, button_tick1, NULL);
@@ -505,10 +249,13 @@ void app_main(void)
 
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
-    //ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
 
+#ifdef CONFIG_FROM_MORKOVKA_WI_FI_TYPE
     wifi_init_softap();
-    //ESP_ERROR_CHECK(example_connect());
+#else
+    ESP_ERROR_CHECK(example_connect());
+#endif
 
 #if !CONFIG_IDF_TARGET_LINUX
 #ifdef CONFIG_EXAMPLE_CONNECT_WIFI
@@ -526,7 +273,6 @@ void app_main(void)
 
     while (server) {
         sleep(5);
-        //button_tick();
     }
 }
 

461b7fa91adae7ca62859bacd8b324d3b94abdd3
Morkovka21Vek <142694961+Morkovka21Vek@users.noreply.github.com>
2025-01-11 13:26:40 +0300

    Исправдено рисование, хоткеи, в player ещё и клавиши(вверх и вниз) красивый код в assets.cpp

--- ping_pong/game/assets.cpp
+++ ping_pong/game/assets.cpp
@@ -1,4 +1,9 @@
 #include "include/assets.h"
+#include <iostream>
+
+#define char_size(ch, xsize, ysize)  xsize = sizeof(ch[0])/ sizeof(char);\
+                       ysize = sizeof(ch)/ sizeof(ch[0]);
+
 
 const char zeroChar[6][4] = {
   {'0', '0', '0', '0'},
@@ -161,92 +166,52 @@ const char plusChar[6][6] = {
   {' ', ' ', '+', '+', ' ', ' '}
 };
 
-char getSymbolCh (enum charsEnum ch, int x, int y) {
+char getSymbolCh (char ch, int x, int y) {
   char out = 'E';
 
-  //int rows = sizeof(tempCh)/ sizeof(tempChar[0]);
-  //int cols = sizeof(tempCh[0])/ sizeof(int);
   switch (ch) {
-    case Char_ball: out = ballChar[y][x]; break;
-    case Char_0: out = zeroChar[y][x]; break;
-    case Char_1: out = oneChar[y][x]; break;
-    case Char_2: out = twoChar[y][x]; break;
-    case Char_3: out = threeChar[y][x]; break;
-    case Char_colon: out = colonChar[y][x]; break;
-    case Char_P: out = Pchar[y][x]; break;
-    case Char_L: out = Lchar[y][x]; break;
-    case Char_A: out = Achar[y][x]; break;
-    case Char_Y: out = Ychar[y][x]; break;
-    case Char_E: out = Echar[y][x]; break;
-    case Char_R: out = Rchar[y][x]; break;
-    case Char_O: out = Ochar[y][x]; break;
-    case Char_I: out = Ichar[y][x]; break;
-    case Char_N: out = Nchar[y][x]; break;
-    case Char_T: out = Tchar[y][x]; break;
-    case Char_W: out = Wchar[y][x]; break;
-    case Char_plus: out = plusChar[y][x]; break;
+    case  9 : out = ballChar[y][x];  break; // шарик
+    case '0': out = zeroChar[y][x];  break;
+    case '1': out = oneChar[y][x];   break;
+    case '2': out = twoChar[y][x];   break;
+    case '3': out = threeChar[y][x]; break;
+    case ':': out = colonChar[y][x]; break;
+    case 'P': out = Pchar[y][x];     break;
+    case 'L': out = Lchar[y][x];     break;
+    case 'A': out = Achar[y][x];     break;
+    case 'Y': out = Ychar[y][x];     break;
+    case 'E': out = Echar[y][x];     break;
+    case 'R': out = Rchar[y][x];     break;
+    case 'O': out = Ochar[y][x];     break;
+    case 'I': out = Ichar[y][x];     break;
+    case 'N': out = Nchar[y][x];     break;
+    case 'T': out = Tchar[y][x];     break;
+    case 'W': out = Wchar[y][x];     break;
+    case '+': out = plusChar[y][x];  break;
   }
   return out;
 }
 
-void getSizeCh (enum charsEnum ch, int& xSize, int& ySize) {
-  //int rows = sizeof(tempCh)/ sizeof(tempChar[0]);
-  //int cols = sizeof(tempCh[0])/ sizeof(int);
+void getSizeCh (char ch, int& xSize, int& ySize) {
   switch (ch) {
-    case Char_ball:
-      xSize = sizeof(ballChar[0])/ sizeof(char);
-      ySize = sizeof(ballChar)/ sizeof(ballChar[0]); break;
-    case Char_0:
-      xSize = sizeof(zeroChar[0])/ sizeof(char);
-      ySize = sizeof(zeroChar)/ sizeof(zeroChar[0]); break;
-    case Char_1:
-      xSize = sizeof(oneChar[0])/ sizeof(char);
-      ySize = sizeof(oneChar)/ sizeof(oneChar[0]); break;
-    case Char_2:
-      xSize = sizeof(twoChar[0])/ sizeof(char);
-      ySize = sizeof(twoChar)/ sizeof(twoChar[0]); break;
-    case Char_3:
-      xSize = sizeof(threeChar[0])/ sizeof(char);
-      ySize = sizeof(threeChar)/ sizeof(threeChar[0]); break;
-    case Char_colon:
-      xSize = sizeof(colonChar[0])/ sizeof(char);
-      ySize = sizeof(colonChar)/ sizeof(colonChar[0]); break;
-    case Char_P:
-      xSize = sizeof(Pchar[0])/ sizeof(char);
-      ySize = sizeof(Pchar)/ sizeof(Pchar[0]); break;
-    case Char_L:
-      xSize = sizeof(Lchar[0])/ sizeof(char);
-      ySize = sizeof(Lchar)/ sizeof(Lchar[0]); break;
-    case Char_A:
-      xSize = sizeof(Achar[0])/ sizeof(char);
-      ySize = sizeof(Achar)/ sizeof(Achar[0]); break;
-    case Char_Y:
-      xSize = sizeof(Ychar[0])/ sizeof(char);
-      ySize = sizeof(Ychar)/ sizeof(Ychar[0]); break;
-    case Char_E:
-      xSize = sizeof(Echar[0])/ sizeof(char);
-      ySize = sizeof(Echar)/ sizeof(Echar[0]); break;
-    case Char_R:
-      xSize = sizeof(Echar[0])/ sizeof(char);
-      ySize = sizeof(Echar)/ sizeof(Echar[0]); break;
-    case Char_O:
-      xSize = sizeof(Ochar[0])/ sizeof(char);
-      ySize = sizeof(Ochar)/ sizeof(Ochar[0]); break;
-    case Char_I:
-      xSize = sizeof(Ichar[0])/ sizeof(char);
-      ySize = sizeof(Ichar)/ sizeof(Ichar[0]); break;
-    case Char_N:
-      xSize = sizeof(Nchar[0])/ sizeof(char);
-      ySize = sizeof(Nchar)/ sizeof(Nchar[0]); break;
-    case Char_T:
-      xSize = sizeof(Tchar[0])/ sizeof(char);
-      ySize = sizeof(Tchar)/ sizeof(Tchar[0]); break;
-    case Char_W:
-      xSize = sizeof(Wchar[0])/ sizeof(char);
-      ySize = sizeof(Wchar)/ sizeof(Wchar[0]); break;
-    case Char_plus:
-      xSize = sizeof(plusChar[0])/ sizeof(char);
-      ySize = sizeof(plusChar)/ sizeof(plusChar[0]); break;
+    case  9 : char_size (ballChar,  xSize, ySize); break; // шарик
+    case '0': char_size (zeroChar,  xSize, ySize); break;
+    case '1': char_size (oneChar,   xSize, ySize); break;
+    case '2': char_size (twoChar,   xSize, ySize); break;
+    case '3': char_size (threeChar, xSize, ySize); break;
+    case ':': char_size (colonChar, xSize, ySize); break;
+    case 'P': char_size (Pchar,     xSize, ySize); break;
+    case 'L': char_size (Lchar,     xSize, ySize); break;
+    case 'A': char_size (Achar,     xSize, ySize); break;
+    case 'Y': char_size (Ychar,     xSize, ySize); break;
+    case 'E': char_size (Echar,     xSize, ySize); break;
+    case 'R': char_size (Rchar,     xSize, ySize); break;
+    case 'O': char_size (Ochar,     xSize, ySize); break;
+    case 'I': char_size (Ichar,     xSize, ySize); break;
+    case 'N': char_size (Nchar,     xSize, ySize); break;
+    case 'T': char_size (Tchar,     xSize, ySize); break;
+    case 'W': char_size (Wchar,     xSize, ySize); break;
+    case '+': char_size (plusChar,  xSize, ySize); break;
   }
 }
 

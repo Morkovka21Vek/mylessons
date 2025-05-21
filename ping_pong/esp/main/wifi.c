86a5274828ef8b1992035ee4c3994914a69ac015
Morkovka21Vek <142694961+Morkovka21Vek@users.noreply.github.com>
2025-05-21 19:34:23 +0000

    style: apply clang-format [bot]

--- ping_pong/esp/main/wifi.c
+++ ping_pong/esp/main/wifi.c
@@ -1,47 +1,45 @@
-#include <esp_log.h>
 #include "esp_mac.h"
 #include "esp_wifi.h"
+#include <esp_log.h>
 
 #define ESP_WIFI_SSID "ESP32_AP"
 #define ESP_WIFI_CHANNEL 9
-#define MAX_STA_CONN  5
+#define MAX_STA_CONN 5
 
 static const char *TAG = "wifi";
 
-static void wifi_event_handler(void* arg, esp_event_base_t event_base,
-                                    int32_t event_id, void* event_data)
-{
+static void wifi_event_handler(void *arg, esp_event_base_t event_base,
+                               int32_t event_id, void *event_data) {
     if (event_id == WIFI_EVENT_AP_STACONNECTED) {
-        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
-        ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
-                 MAC2STR(event->mac), event->aid);
+        wifi_event_ap_staconnected_t *event =
+            (wifi_event_ap_staconnected_t *)event_data;
+        ESP_LOGI(TAG, "station " MACSTR " join, AID=%d", MAC2STR(event->mac),
+                 event->aid);
     } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
-        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
-        ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d, reason=%d",
+        wifi_event_ap_stadisconnected_t *event =
+            (wifi_event_ap_stadisconnected_t *)event_data;
+        ESP_LOGI(TAG, "station " MACSTR " leave, AID=%d, reason=%d",
                  MAC2STR(event->mac), event->aid, event->reason);
     }
 }
 
-void wifi_init_softap(void)
-{
+void wifi_init_softap(void) {
     esp_netif_create_default_wifi_ap();
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
-                                                        ESP_EVENT_ANY_ID,
-                                                        &wifi_event_handler,
-                                                        NULL,
-                                                        NULL));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(
+        WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL, NULL));
 
     wifi_config_t wifi_config = {
-        .ap = {
-            .ssid = ESP_WIFI_SSID,
-            .channel = ESP_WIFI_CHANNEL,
-            .max_connection = MAX_STA_CONN,
-            .authmode = WIFI_AUTH_OPEN,
-        },
+        .ap =
+            {
+                .ssid = ESP_WIFI_SSID,
+                .channel = ESP_WIFI_CHANNEL,
+                .max_connection = MAX_STA_CONN,
+                .authmode = WIFI_AUTH_OPEN,
+            },
     };
 
     esp_wifi_set_mode(WIFI_MODE_AP);

9b607c5d8996fca992c6a8d52c35bb187ee3fe3e
Morkovka21Vek <morkovka21vek@mail.ru>
2025-07-06 12:03:59 +0300

    add 6 ex

---
+++ book/p8/6/src/main.cpp
@@ -0,0 +1,39 @@
+#include "Time.hpp"
+#include <iostream>
+
+int main() {
+    {
+        const Time t1(1, 2, 3);
+        const Time t2(3, 2, 1);
+        std::cout << t1 << " + " << t2 << " = " << t1 + t2 << std::endl;
+    }
+
+    {
+        Time t1(1, 0, 59);
+        Time t2(3, 1, 61);
+        std::cout << t1 << " + " << t2 << " = " << t1 + t2 << std::endl;
+        t1++;
+        ++t2;
+        std::cout << t1 << ' ' << t2 << std::endl;
+    }
+
+    {
+        const Time t1(1, 4000000180, 400000072);
+        std::cout << t1 << std::endl;
+    }
+
+    {
+        const Time t1(3, 2, 1);
+        const Time t2(1, 2, 3);
+        std::cout << t1 << " - " << t2 << " = " << t1 - t2 << std::endl;
+    }
+
+    {
+        const Time t(1, 2, 3);
+        float mul = 5;
+        std::cout << t << " * " << mul << " = " << t * mul << std::endl;
+    }
+
+
+    return 0;
+}

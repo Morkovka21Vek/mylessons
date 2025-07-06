00aedc0e7f82d77440f9111d741b683751ac5a50
Morkovka21Vek <morkovka21vek@mail.ru>
2025-07-06 11:46:13 +0300

    fix: Fix 5 ex

--- book/p8/5/src/Time.cpp
+++ book/p8/5/src/Time.cpp
@@ -4,12 +4,7 @@
 Time::Time() : _hours(0), _min(0), _sec(0) {}
 
 Time::Time(unsigned int hours, unsigned int min, unsigned int sec)
-    : _hours(hours), _min(min), _sec(sec) {
-    this->_min += this->_sec / 60;
-    this->_sec %= 60;
-    this->_hours += this->_min / 60;
-    this->_min %= 60;
-}
+    : _hours(hours), _min(min), _sec(sec) { normalize(); }
 
 std::ostream &operator<<(std::ostream &os, const Time &obj) {
     return os << std::format("{:02}:{:02}:{:02}", obj._hours, obj._min,
@@ -21,27 +16,32 @@ Time operator+(const Time &obj1, const Time &obj2) {
     unsigned int min = obj1._min + obj2._min;
     unsigned int sec = obj1._sec + obj2._sec;
 
-    min += sec / 60;
-    sec %= 60;
-    hours += min / 60;
-    min %= 60;
-
     return Time(hours, min, sec);
 }
 
 Time& Time::operator++() {
     this->_min++;
+    normalize();
     return *this;
 }
 Time& Time::operator--() {
     this->_min--;
+    normalize();
     return *this;
 }
 Time& Time::operator++(int) {
     ++this->_min;
+    normalize();
     return *this;
 }
 Time& Time::operator--(int) {
     --this->_min;
+    normalize();
     return *this;
+}
+void Time::normalize() {
+    this->_min += this->_sec / 60;
+    this->_sec %= 60;
+    this->_hours += this->_min / 60;
+    this->_min %= 60;
 }
